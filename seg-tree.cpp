e0ad8c9d292721d3706dabaf6737946a0e539cec
Matheus Ataide <matheusataide95@gmail.com>
2021-10-04 21:57:48 -0300

    Improving segtree

--- seg-tree.cpp
+++ seg-tree.cpp
@@ -4,29 +4,47 @@
 
 typedef long long ll;
 
-ll tree[4 * MAXN];
+class SegTree {
+private:
+	int n;
+	vector<ll> tree;
+	
+	void _update(int root, int l, int r, int p, int val) {
+		if (p > r || p < l || l > r) return;
+		if (l == r) {
+			tree[root] = val;
+		} else {
+			int mid = (l + r) / 2;
+			update(left(root), l, mid, p, val);
+			update(right(root), mid + 1, r, p, val);
+			tree[root] = max(tree[left(root)], tree[right(root)]);
+		}
+	}
 
-void update(int root, int l, int r, int p, int val) {
-    if (p > r || p < l || l > r) return;
-    if (l == r) {
-        tree[root] = val;
-    } else {
-        int mid = (l + r) / 2;
-        update(left(root), l, mid, p, val);
-        update(right(root), mid + 1, r, p, val);
-        tree[root] = max(tree[left(root)], tree[right(root)]);
-    }
-}
+	ll _get(int root, int l, int r, int a, int b) {
+		if (a > r || b < l) return 0;
+		if (l == a && r == b) {
+			return tree[root];
+		} else {
+			int mid = (l + r) / 2;
+			return max(_get(left(root), l, mid, a, min(mid, b)), _get(right(root), mid + 1, r, max(mid+1, a), b));
+		}
+	}
+	
+public:
+	SegTree(int n) : n(n) {
+		tree.assign(4 * n, 0);
+	}
+	
+	void update(int p, int val) {
+		_update(0, 0, n - 1, p, val);
+	}
+	
+	int get(int a, int b) {
+		return _get(0, 0, n - 1, a, b);
+	}
+};
 
-ll getMax(int root, int l, int r, int a, int b) {
-    if (a > r || b < l) return 0;
-    if (l == a && r == b) {
-        return tree[root];
-    } else {
-        int mid = (l + r) / 2;
-        return max(getMax(left(root), l, mid, a, min(mid, b)), getMax(right(root), mid + 1, r, max(mid+1, a), b));
-    }
-}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////

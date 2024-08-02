14d4773b80a466f2b810d90bdf41647548bfac41
Matheus Ataide <matheusataide95@gmail.com>
2024-08-02 10:57:55 +0100

    Update seg-tree.cpp

--- seg-tree.cpp
+++ seg-tree.cpp
@@ -5,44 +5,45 @@
 typedef long long ll;
 
 class SegTree {
-private:
-	int n;
-	vector<ll> tree;
-	
-	void _update(int root, int l, int r, int p, int val) {
-		if (p > r || p < l || l > r) return;
-		if (l == r) {
-			tree[root] = val;
-		} else {
-			int mid = (l + r) / 2;
-			update(left(root), l, mid, p, val);
-			update(right(root), mid + 1, r, p, val);
-			tree[root] = max(tree[left(root)], tree[right(root)]);
-		}
-	}
+    private:
+        int n;
+        vector<ll> tree;
+        
+        void _update(int root, int l, int r, int p, ll val) {
+            if (p > r || p < l || l > r) return;
+            if (l == r) {
+                tree[root] = val;
+            } else {
+                int mid = (l + r) / 2;
+                _update(left(root), l, mid, p, val);
+                _update(right(root), mid + 1, r, p, val);
+                tree[root] = tree[left(root)] + tree[right(root)];
+            }
+        }
 
-	ll _get(int root, int l, int r, int a, int b) {
-		if (a > r || b < l) return 0;
-		if (l == a && r == b) {
-			return tree[root];
-		} else {
-			int mid = (l + r) / 2;
-			return max(_get(left(root), l, mid, a, min(mid, b)), _get(right(root), mid + 1, r, max(mid+1, a), b));
-		}
-	}
-	
-public:
-	SegTree(int n) : n(n) {
-		tree.assign(4 * n, 0);
-	}
-	
-	void update(int p, int val) {
-		_update(0, 0, n - 1, p, val);
-	}
-	
-	int get(int a, int b) {
-		return _get(0, 0, n - 1, a, b);
-	}
+        ll _get(int root, int l, int r, int a, int b) {
+            if (a > r || b < l) return 0;
+            if (l == a && r == b) {
+                return tree[root];
+            } else {
+                int mid = (l + r) / 2;
+                return _get(left(root), l, mid, a, min(mid, b)) + 
+                    _get(right(root), mid + 1, r, max(mid + 1, a), b);
+            }
+        }
+        
+    public:
+        SegTree(int n) : n(n) {
+            tree.resize(4 * n);
+        }
+        
+        void update(int p, ll val) {
+            _update(0, 0, n - 1, p, val);
+        }
+        
+        ll get(int a, int b) {
+            return _get(0, 0, n - 1, a, b);
+        }
 };
 
 
@@ -59,39 +60,56 @@ public:
 
 typedef long long ll;
 
-ll tree[4 * MAXN];
-ll lazy[4 * MAXN];
-
-void prop(int root, int l, int r) {
-    tree[root] += lazy[root] * (r - l + 1);
-    if (l != r) {
-        lazy[left(root)] += lazy[root];
-        lazy[right(root)] += lazy[root];
-    }
-    lazy[root] = 0;
-}
-
-void update(int root, int l, int r, int a, int b, ll val) {
-    prop(root, l, r);
-    if (a > r || b < l) return;
-    if (l == a && r == b) {
-        lazy[root] += val;
-        prop(root, l, r);
-    } else {
-        int mid = (l + r) / 2;
-        update(left(root), l, mid, a, min(mid, b), val);
-        update(right(root), mid + 1, r, max(mid + 1, a), b, val);
-        tree[root] = tree[left(root)] + tree[right(root)];
-    }
-}
-
-ll getSum(int root, int l, int r, int a, int b) {
-    prop(root, l, r);
-    if (a > r || b < l) return 0;
-    if (l == a && r == b) {
-        return tree[root];
-    } else {
-        int mid = (l + r) / 2;
-        return getSum(left(root), l, mid, a, min(mid, b)) + getSum(right(root), mid + 1, r, max(mid + 1, a), b);
-    }
-}
+class SegTree {
+    private:
+        int n;
+        vector<ll> tree;
+        vector<ll> lazy;
+ 
+        void prop(int root, int l, int r) {
+            tree[root] += (r - l + 1) * lazy[root];
+            if (l != r) {
+                lazy[left(root)] += lazy[root];
+                lazy[right(root)] += lazy[root];
+            }
+            lazy[root] = 0;
+        }
+ 
+        void _update(int root, int l, int r, int a, int b, ll val) {
+            prop(root, l, r);
+            if (a > r || b < l) return;
+            if (l == a && r == b) {
+                lazy[root] += val;
+                prop(root, l, r);
+                return;
+            }
+            int mid = (l + r) / 2;
+            _update(left(root), l, mid, a, min(mid, b), val);
+            _update(right(root), mid + 1, r, max(mid + 1, a), b, val);
+        }
+ 
+        ll _get(int root, int l, int r, int a, int b) {
+            prop(root, l, r);
+            if (a > r || b < l) return 0;
+            if (l == a && r == b) {
+                return tree[root];
+            }
+            int mid = (l + r) / 2;
+            return _get(left(root), l, mid, a, min(mid, b)) +
+                   _get(right(root), mid + 1, r, max(mid +1, a), b);
+        }
+ 
+    public:
+        SegTree(int n) : n(n) {
+            tree.resize(4 * n);
+            lazy.resize(4 * n);
+        }
+ 
+        void update(int a, int b, ll val) {
+            _update(0, 0, n - 1, a, b, val);
+        }
+ 
+        ll get(int a, int b) {
+            return _get(0, 0, n - 1, a, b);
+        }
+};
